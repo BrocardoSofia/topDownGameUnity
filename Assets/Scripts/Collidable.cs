94064c529f252984bbb4d81701439e6b0fde6d69
BrocardoSofia <sofia.brocardo@hotmail.com>
2023-07-16 19:19:52 -0300

    update collision script

--- Assets/Scripts/Collidable.cs
+++ Assets/Scripts/Collidable.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class Collidable : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public ContactFilter2D filter;
+    private BoxCollider2D boxCollider;
+    private Collider2D[] hits = new Collider2D[10];
+
+    protected virtual void Start()
     {
-        
+        boxCollider = GetComponent<BoxCollider2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    protected virtual void Update()
     {
-        
+        //Collision work
+        boxCollider.OverlapCollider(filter, hits);
+
+        for (int i = 0; i < hits.Length; i++)
+        {
+            if (hits[i] == null)
+                continue;
+
+            Debug.Log(hits[i].name);
+
+            //clean the array
+            hits[i] = null;
+        }
     }
+
 }
