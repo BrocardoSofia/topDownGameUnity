32aec8af07d9b971f6158fd7748967461755d1ed
BrocardoSofia <sofia.brocardo@hotmail.com>
2023-07-18 02:33:52 -0300

    update

--- Assets/Scripts/Portal.cs
+++ Assets/Scripts/Portal.cs
@@ -5,14 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class Portal : Collidable
 {
-    public string[] sceneNames;
+    public string sceneName;
 
     protected override void OnCollide(Collider2D coll)
     {
         if (coll.name == "Player")
         {
             //Teleport the player
-            string sceneName = sceneNames[Random.Range(0, sceneNames.Length)];
+            //string sceneName = sceneNames[Random.Range(0, sceneNames.Length)];
             SceneManager.LoadScene(sceneName);
         }
     }
