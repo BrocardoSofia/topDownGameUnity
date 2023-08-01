3e08c5f4d644c159383fbd015a17a0300e85f353
BrocardoSofia <sofia.brocardo@hotmail.com>
2023-08-01 20:34:51 -0300

    update

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -48,7 +48,7 @@ public class Weapon : Collidable
             if(coll.name != "Player")
             {   
                 //Create a new damage object, then we`ll send it to the fighter we've hit
-                Damage dmg = new Damage()
+                Damage dmg = new Damage
                 {
                     damageAmount = damagePoint,
                     origin = transform.position,
