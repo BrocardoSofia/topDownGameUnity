fdded78173dedb5382a6092b86bdea1a8d9d20e3
BrocardoSofia <sofia.brocardo@hotmail.com>
2023-08-06 15:38:43 -0300

    update weapon code

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -13,13 +13,15 @@ public class Weapon : Collidable
     private SpriteRenderer spriteRenderer;
 
     //Swing
+    private Animator anim;
     private float coolDown = 0.5f;
     private float lastSwing;
 
     protected override void Start()
     {
         base.Start();
-        spriteRenderer = GetComponent<SpriteRenderer>();   
+        spriteRenderer = GetComponent<SpriteRenderer>();  
+        anim = GetComponent<Animator>();
     }
 
     protected override void Update()
@@ -38,7 +40,7 @@ public class Weapon : Collidable
 
     private void Swing()
     {
-        Debug.Log("Swing");
+        anim.SetTrigger("Swing");
     }
 
     protected override void OnCollide(Collider2D coll)
